44e2431d8ad25cc93036ea585b322d52176e3396
yechencheng <yechencheng@gmail.com>
2014-12-07 14:53:48 -0500

    Add PowerStreamBypass

---
+++ PowerStreamData.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+const int SIZE = 1 << 30;
+
+double A[SIZE];
+double B[SIZE];
+
+int main(){
+	__protected_store_stream_set(FORWARD, &a, 0);
+	//__protected_stream_count_depth(SIZE*sizeof(double)/128, DEEPER, 0);
+	__protected_stream_count_depth(1023, DEEPER, 0);
+	__protected_stream_set(FORWARD, &b, 1);
+	__transient_protected_stream_count_depth(1023, DEEPER, 1);
+
+	for(int i = 0; i < 10; i++)
+		for(int j = 0; j < SIZE; j++){
+			A[i] += B[i];
+		}
+
+	return 0;
+}
