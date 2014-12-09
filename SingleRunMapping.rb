1c5f3654b63df527de46d3bcfc8af55b5aa8386a
yechencheng <yechencheng@gmail.com>
2014-12-08 21:53:22 -0500

    6

--- SingleRunMapping.rb
+++ SingleRunMapping.rb
@@ -5,6 +5,11 @@ for i in [4,6,8,12]
 		if j.to_s(2).count("1") != i
 			next
 		end
-		system("taskset #{j} parsecmgmt -a run -p fluidanimate -n #{i} -i native > #{i}_#{j}.log")
+		hexj = j.to_s(16)
+		puts "#{i} #{hexj} : taskset #{hexj} parsecmgmt -a run -p fluidanimate -n #{i} -i native > #{i}_#{hexj}.log"
+		if File.exist?("#{i}_#{hexj}.log")
+			next
+		end
+		system("taskset #{hexj} parsecmgmt -a run -p fluidanimate -n #{i} -i native > #{i}_#{hexj}.log")
 	end
 end
